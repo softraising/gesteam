54e64505edbb61ad91d95a003e6d4de5c08efefa
jamarin6 <jamarinhidalgo@gmail.com>
2014-06-10 16:26:00 +0000

    admin routes added

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 NewGesteam::Application.routes.draw do
   get "welcome/index"
 
-
+  resources :admins
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
