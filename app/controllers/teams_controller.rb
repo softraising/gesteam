1b3d3fa47b4a06201826a9cae7fe7b3804326070
jamarin6 <jamarinhidalgo@gmail.com>
2014-06-11 10:56:58 +0000

    created a button to create teams and show teams in team index view

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,4 +1,20 @@
 class TeamsController < ApplicationController
 	def index
+		@teams = current_admin.teams
+	end
+
+	def new
+		@team = current_admin.teams.new
+	end
+
+	def create
+		@team = current_admin.teams.create entry_params
+		redirect_to teams_path
+	end
+
+	private
+
+	def entry_params
+		params.require(:team).permit(:name, :description)
 	end
 end
