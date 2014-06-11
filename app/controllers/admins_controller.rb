e0523f5d41fd8c95f7d57c5b4e9cb93cf73cf480
jamarin6 <jamarinhidalgo@gmail.com>
2014-06-11 16:56:27 +0000

    errors in sign up fixed

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -7,7 +7,7 @@ class AdminsController < ApplicationController
 	def create
 	  @admin = Admin.new(admin_params)
 	  if @admin.save
-	    redirect_to teams_path, :notice => "Signed up!"
+	    redirect_to sessions_new_path, :notice => "Signed up!"
 	  else
 	    render "new"
 	  end
