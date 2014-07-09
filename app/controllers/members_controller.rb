ff8fc3cd2aae8767ad3bd388b788d35bc40cd095
jamarin6 <jamarinhidalgo@gmail.com>
2014-07-09 18:23:39 +0000

    add members to Teams

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,2 +1,23 @@
 class MembersController < ApplicationController
+
+	def index
+		@team = Team.find(params[:team_id])
+		@members = @team.members
+	end
+	def new
+		team = Team.find(params[:team_id])
+		@member = team.members.new
+	end
+
+	def create
+		@team = Team.find(params[:team_id])
+		@team.members.create entry_params
+		redirect_to teams_path
+	end
+
+	def entry_params
+		params.require(:member).permit(:name, :email, :incidences, :team_id)
+	end
+
+
 end
