64faba89dc3a9114d3262b4cee000f57883c7a74
Jose Izquierdo <jainguns@gmail.com>
2014-07-26 19:15:28 +0200

    Test 'it is invalid without an email account' for admin_model on red

--- spec/models/admin_spec.rb
+++ spec/models/admin_spec.rb
@@ -6,4 +6,8 @@ describe Admin do
 	it 'has a valid factory' do
 		FactoryGirl.create(:admin).should be_valid
 	end
+
+	it 'is invalid without an email account' do
+		FactoryGirl.build(:admin, email: nil).should be_valid
+	end
 end
