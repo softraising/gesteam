4bed690789198b9e36333bf0f4134aaf1e2a5225
Jose Izquierdo <jainguns@gmail.com>
2014-07-26 18:34:28 +0200

    Test 'it gets a HTTP status response 200' on welcome_controller on red.

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -8,5 +8,11 @@ describe WelcomeController do
   		get :index
   		expect(response).to render_template("index")
   	end
+
+  	it "gets a HTTP status response 200" do
+  		get :index
+  		expect(response.status). to eq(300)
+  		expect(response). to be_success
+    end
   end
 end
