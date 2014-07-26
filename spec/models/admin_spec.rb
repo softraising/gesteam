5886c4cc298c5f431c5cb66b4ce9485d11e4bff3
Jose Izquierdo <jainguns@gmail.com>
2014-07-26 19:21:41 +0200

    Test 'it is invalid without a password' for admin_model on red

--- spec/models/admin_spec.rb
+++ spec/models/admin_spec.rb
@@ -10,4 +10,8 @@ describe Admin do
 	it 'is invalid without an email account' do
 		FactoryGirl.build(:admin, email: nil).should_not be_valid
 	end
+
+	it 'is invalid without a password' do
+		FactoryGirl.build(:admin, password: nil).should be_valid
+	end
 end
