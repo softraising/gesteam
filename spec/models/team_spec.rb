63fd812ab45098c18848b03b5d13984c0e523d44
Jose Izquierdo <jainguns@gmail.com>
2014-07-27 13:23:08 +0200

    Test 'it is invalid without a name' on team_model is red

--- spec/models/team_spec.rb
+++ spec/models/team_spec.rb
@@ -6,4 +6,8 @@ describe Team do
 	it 'has a valid factory' do
 		FactoryGirl.create(:team).should be_valid
 	end
+
+	it 'is invalid without a name' do
+		FactoryGirl.build(:team, name: nil).should be_valid
+	end
 end
